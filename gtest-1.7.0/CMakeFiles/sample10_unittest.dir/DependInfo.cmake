81461124210d20670984dbf4f6f68df9f2164f52
Ari Croock <aricroock@gmail.com>
2014-03-30 17:43:18 +0200

    gtest library

---
+++ gtest-1.7.0/CMakeFiles/sample10_unittest.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ari/development/ELEN2004/ELEN2004_project_2014/gtest-1.7.0/samples/sample10_unittest.cc" "/home/ari/development/ELEN2004/ELEN2004_project_2014/gtest-1.7.0/CMakeFiles/sample10_unittest.dir/samples/sample10_unittest.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ari/development/ELEN2004/ELEN2004_project_2014/gtest-1.7.0/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ gtest-1.7.0/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ari/development/ELEN2004/ELEN2004_project_2014/gtest-1.7.0
+# Build directory: /home/ari/development/ELEN2004/ELEN2004_project_2014/gtest-1.7.0
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
