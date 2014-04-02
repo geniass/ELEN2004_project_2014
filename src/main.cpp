353c2ce46b62003646455e04cb59779ac7f033ac
Ari Croock <aricroock@gmail.com>
2014-04-02 22:48:32 +0200

    moved hourly stuff into sruct

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,73 @@ double JtokWh(double);
 void writeStatsFile(double, double, double);
 void writeHourFile(const map<int, double>&);
 
+struct HourStruct
+{
+    double hourEnergy; // total energy for current hour
+    int hourCounter; // current hour
+    map<int, double> hourMap;
+
+    void initMap()
+    {
+        for(int i = 0; i < 24; i++)
+        {
+            // Initialise the map to have 24 pairs (0..23) initialised to 0.0
+            hourMap.insert(pair<int,double>(i, 0.0));
+        }
+    }
+
+    // Given the change in hours and the energy from the previous datapoint to
+    // the current one, update the map accordingly
+    void update(int deltaHour, double energy)
+    {
+        if(deltaHour > 0)
+        {
+            hourMap.at(hourCounter) = JtokWh(hourEnergy);
+            // If there is a gap in the measurements, this prevents
+            // the gap from being ignored, ie, there will also be
+            // an appropriate gap in the byhour.txt file
+            hourCounter += deltaHour;
+            hourEnergy = energy;
+        }
+        else if(deltaHour == 0)
+        {
+            hourEnergy += energy;
+        }
+
+    }
+
+    // checks if the map has ever been updated (more than 0 hours have passed).
+    void finalUpdate(double totalEnergy)
+    {
+        if(hourCounter == 0)
+        {
+            // hourCounter wasn't incremented, implying that there's only
+            // 1 hour in the file. THerefore energy for this hour is just
+            // totalEnergy
+            hourMap.at(hourCounter) = JtokWh(totalEnergy);
+        }
+    }
+
+    void writeHourFile()
+    {
+        ofstream hourFile("byhour.txt");
+        if(hourFile.is_open())
+        {
+            for(map<int,double>::const_iterator i = hourMap.begin(); i != hourMap.end(); i++)
+            {
+                hourFile << i->first << " " << i->second << endl;
+            }
+        }
+        else
+        {
+            cerr << "ERROR: Could not open byhour.txt" << endl;
+        }
+        hourFile.close();
+    }
+
+    HourStruct() : hourEnergy(0), hourCounter(0), hourMap() { initMap(); }
+};
+
 int main()
 {
     ifstream dataFile("data.txt");
@@ -31,10 +98,7 @@ int main()
     double maxPower = 0.0;
     double totalEnergy = 0.0;
 
-    double hourEnergy = 0.0; // total energy for current hour
-    int hourCounter = 0; // current hour
-    map<int, double> hourMap;
-    initialiseHourMap(hourMap);
+    HourStruct hourStruct;
 
     DataPoint previousDataPoint;
 
@@ -61,19 +125,8 @@ int main()
                     totalEnergy += energy;
 
                     int deltaHour = DataPoint::compareHours(currentDataPoint, previousDataPoint);
-                    if(deltaHour > 0)
-                    {
-                        hourMap.at(hourCounter) = JtokWh(hourEnergy);
-                        // If there is a gap in the measurements, this prevents
-                        // the gap from being ignored, ie, there will also be
-                        // an appropriate gap in the byhour.txt file
-                        hourCounter += deltaHour;
-                        hourEnergy = energy;
-                    }
-                    else if(deltaHour == 0)
-                    {
-                        hourEnergy += energy;
-                    }
+
+                    hourStruct.update(deltaHour, energy);
                 }
 
                 // if we're on the first line, skip the integration (since it
@@ -88,20 +141,17 @@ int main()
             }
         }
 
-        if(hourCounter == 0)
-        {
-            // hourCounter wasn't incremented, implying that there's only
-            // 1 hour in the file. THerefore energy for this hour is just
-            // totalEnergy
-            hourMap.at(hourCounter) = JtokWh(totalEnergy);
-        }
+        hourStruct.finalUpdate(totalEnergy);
 
-        cout << "Lines: " << lineCount << endl << "Power (W): " << totalPower << endl << "Max power(W): " << maxPower << endl << "energy (J): " << totalEnergy << endl;
+        cout << "Lines: " << lineCount << endl
+            << "Power (W): " << totalPower << endl
+            << "Max power(W): " << maxPower << endl
+            << "energy (J): " << totalEnergy << endl;
 
         writeStatsFile(JtokWh(totalEnergy),            // 1 kWh = 3.6MJ
                        totalPower / lineCount,           // compute the average
                        maxPower);
-        writeHourFile(hourMap);
+        hourStruct.writeHourFile();
     }
     else
     {
@@ -112,15 +162,6 @@ int main()
     dataFile.close();
 
     return 0;
-}
-
-void initialiseHourMap(map<int, double>& hourMap)
-{
-    for(int i = 0; i < 24; i++)
-    {
-        // Initialise the map to have 24 pairs (0..23) initialised to 0.0
-        hourMap.insert(pair<int,double>(i, 0.0));
-    }
 }
 
 double JtokWh(double joules)
@@ -143,21 +184,4 @@ void writeStatsFile(double kWh, double meanPower, double maxPower)
         cerr << "ERROR: Could not open stats.txt" << endl;
     }
     statsFile.close();
-}
-
-void writeHourFile(const map<int,double>& hourMap)
-{
-    ofstream hourFile("byhour.txt");
-    if(hourFile.is_open())
-    {
-        for(map<int,double>::const_iterator i = hourMap.begin(); i != hourMap.end(); i++)
-        {
-            hourFile << i->first << " " << i->second << endl;
-        }
-    }
-    else
-    {
-        cerr << "ERROR: Could not open byhour.txt" << endl;
-    }
-    hourFile.close();
 }
